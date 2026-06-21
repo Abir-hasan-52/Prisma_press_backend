99a5e688fcf770d695f05293e0376b2883cde0b8
Abir Hasan <abirhasan5208@gmail.com>
2026-06-21 23:27:56 +0600

    user registattion and create user profile

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,28 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import { prisma } from "./lib/prisma";
+import { userRouter } from "./modules/user/user.route";
 
 const app: Application = express();
 
 // middle ware
-app.use(cors({
-    origin:config.app_url,
-    credentials:true,
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
+  console.log(user);
+  res.send("hello , world!");
+});
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("hello , world!")
-})
-
+app.use("/api/user", userRouter)
 
 export default app;
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,50 @@
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import config from "../../config";
+import { registerUserPayload } from "./user.interface";
+ 
+
+const registerUserIntoDb = async (payload:registerUserPayload) => {
+  const { name, email, password, profilePhoto } = payload;
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (isUserExist) {
+    throw new Error("User with this email already exists");
+  }
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const CreatedUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  await prisma.profile.create({
+    data: {
+      userId: CreatedUser.id,
+      profilePhoto,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: CreatedUser.id,
+      email: CreatedUser.email || email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+  return user
+};
+
+export const userService={
+    registerUserIntoDb
+}
