f80bfd607d202fcc29e0401ca7e0557468aabd0a
Abir Hasan <abirhasan5208@gmail.com>
2026-06-24 23:56:50 +0600

    create jwt_access and refresh token for user validatation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const loginUserFromDB = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -31,7 +34,49 @@ const loginUserFromDB = async (payload: ILoginUser) => {
   if (!isPasswordMatched) {
     throw new Error("Password is incorrect");
   }
-  return user;
+
+  const jwtPayload = {
+    id: (await user).id,
+    name: (await user).name,
+    email: (await user).email,
+    role: (await user).role,
+  };
+
+  //   const accessToken = jwt.sign(
+  //     {
+  //       jwtPayload,
+  //     },
+  //     config.jwt_access_secret,
+  //     {
+  //       expiresIn: config.jwt_access_expires_in,
+  //     } as SignOptions,
+  //   );
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+//   const refreshToken = jwt.sign(
+//     {
+//       jwtPayload,
+//     },
+//     config.jwt_refresh_secret,
+//     {
+//       expiresIn: config.jwt_refresh_expires_in,
+//     } as SignOptions,
+//   );
+
+const refreshToken=jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const authService = {
